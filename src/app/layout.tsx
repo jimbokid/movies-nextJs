422c52e3e3b20bfa646b3930c4e14a5f753e80a2
Yurii Kovalchuk <jimbokid89@gmail.com>
2025-10-18 14:30:52 +0300

    [feature] - update styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950`}
         >
         <ScrollToTop/>
         <Header/>
